bcbbe3030bfd06edb62f4599d6a9d03f08ecc54b
On1Key <on1key@foxmail.com>
2018-04-17 18:30:09 +0800

    add overlayer frame animation

--- DMSwipeCards/Classes/DMSwipeCard.swift
+++ DMSwipeCards/Classes/DMSwipeCard.swift
@@ -71,16 +71,16 @@ class DMSwipeCard: UIView {
 			break
 		case .changed:
             //----添加垂直滑动屏蔽-------
-            switch (xFromCenter, yFromCenter) {
-            case let (x, y) where abs(x) >= abs(y) && x > 0:
-                //Right
-                break
-            case let (x, y) where abs(x) >= abs(y) && x < 0:
-                //Left
-                break
-            default:
-                return
-            }
+            //switch (xFromCenter, yFromCenter) {
+            //case let (x, y) where abs(x) >= abs(y) && x > 0:
+              //  //Right
+             //   break
+            //case let (x, y) where abs(x) >= abs(y) && x < 0:
+              //  //Left
+             //   break
+            //default:
+            //    return
+            //}
             //----添加垂直滑动屏蔽-------
 			let rStrength = min(xFromCenter / self.rotationStrength, rotationMax)
 			let rAngle = self.rotationAngle * rStrength
@@ -129,6 +129,81 @@ class DMSwipeCard: UIView {
 		}
 
 		activeOverlay?.alpha = min(fabs(distance)/100, 1.0)
+		
+		//------添加左右动效-------------
+        if let activeView = activeOverlay{
+            
+            //尝试使用convert，失败
+////            let activeW = activeView.frame.size.width
+//            let screenW = UIScreen.main.bounds.size.width
+//
+////            if distance == 0.001 {
+////                activeView.center = CGPoint(x: self.frame.size.width * 0.5, y: activeView.center.y)
+////            }else if distance == -0.001 {
+////                activeView.center = CGPoint(x: 0, y: activeView.center.y)
+////            }
+//
+//            let activeCenterToWindow = activeView.convert(activeView.center, to: UIApplication.shared.keyWindow!)
+//
+//            let proportion = fabs(distance)/(screenW * 0.5)
+//            var activeCenterX:CGFloat =  activeView.center.x
+//            print("\n")
+//            print("activeCenterToWindow=",activeCenterToWindow)
+//            if distance > 0 {
+//                activeCenterX = screenW - fabs(distance)/
+////                if distance >= (self.convert(self.center, to: UIApplication.shared.keyWindow!).x - screenW * 0.5) {
+////                    activeCenterX = self.center.x
+////                }
+//            }else{
+//                activeCenterX = fabs(distance) + 100
+////                if fabs(distance) >= screenW - self.convert(self.center, to: UIApplication.shared.keyWindow!).x {
+////                    activeCenterX = self.center.x
+////                }
+//            }
+////            print(#function,#line,distance,activeCenterX)
+//            let centerFromWindow = activeView.convert(CGPoint(x: activeCenterX, y: activeCenterToWindow.y), from: UIApplication.shared.keyWindow!)
+//            print("distance=",distance)
+//            print("centerFromWindow=",centerFromWindow)
+//            print("rect ori=",activeView.frame)
+//            print("rect con=",activeView.convert(activeView.frame, to: UIApplication.shared.keyWindow!))
+//            UIView .animate(withDuration: 0.01, animations: {
+//                activeView.center = CGPoint(x: activeCenterX, y: activeView.center.y)
+////                activeView.transform = CGAffineTransform.init(scaleX: proportion, y: proportion)
+//            }) { (complete) in
+//
+//            }
+            
+            //改用frame的x值修改动画
+            let activeW = activeView.frame.size.width
+            let centerX = (self.frame.size.width - activeW) * 0.5
+            let screenW = UIScreen.main.bounds.size.width
+            var anchorX:CGFloat = 120*(screenW/375)//需要适配不同手机
+            if (activeView.convert(activeView.bounds, to: UIApplication.shared.keyWindow!).origin.x + activeView.frame.size.width * 0.5) == (activeView.superview!.convert(activeView.superview!.bounds, to: UIApplication.shared.keyWindow!).origin.x + activeView.superview!.frame.size.width * 0.5){
+                anchorX = activeView.convert(activeView.bounds, to: UIApplication.shared.keyWindow!).origin.x + activeView.frame.size.width * 0.5
+            }
+            let proportion = fabs(distance)/anchorX//滑动比例
+            let anchorLeftW = screenW - anchorX
+            var activeX:CGFloat =  0
+            if distance > 0 {
+                activeX = screenW - anchorLeftW * proportion
+                if distance >= anchorX {
+                    activeX = centerX - activeW * 0.5
+                }
+            }else{
+                activeX = anchorLeftW * proportion-activeW
+                if distance <= -anchorX{
+                    activeX = centerX - activeW * 0.5
+                }
+            }
+            print(#function,#line,distance,activeX)
+            UIView .animate(withDuration: 0.01, animations: {
+                activeView.frame = CGRect(x: activeX, y: (activeView.frame.origin.y), width: (activeView.frame.size.width), height: (activeView.frame.size.height))
+                activeView.transform = CGAffineTransform(scaleX: proportion*0.5+0.5, y: proportion*0.5+0.5)
+            }) { (complete) in
+
+            }
+        }
+        //------添加左右动效-------------
 	}
 
 	private func rightAction() {
